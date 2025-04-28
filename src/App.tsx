9d8c1d913e3eb3f8203d3b45d46641654f31664f
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-04-28 09:18:38 +0900

    fix: removed the auth context and modal provider logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,18 @@ import { BrowserRouter } from 'react-router-dom';
 import RouterPage from '@/router/index';
 import ErrorBoundary from '@/components/Error/ErrorBoundary';
 import { RecoilRoot } from 'recoil';
-import { ModalProvider } from '@/components/Modal/ModalProvider';
 import RootLayout from '@/pages/RootLayout';
 
 export default function App() {
     return (
-        <ModalProvider>
-            <RecoilRoot>
-                <ErrorBoundary>
-                    <BrowserRouter>
-                        <RootLayout>
-                            <RouterPage />
-                        </RootLayout>
-                    </BrowserRouter>
-                </ErrorBoundary>
-            </RecoilRoot>
-        </ModalProvider>
+        <RecoilRoot>
+            <ErrorBoundary>
+                <BrowserRouter>
+                    <RootLayout>
+                        <RouterPage />
+                    </RootLayout>
+                </BrowserRouter>
+            </ErrorBoundary>
+        </RecoilRoot>
     );
 }
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,15 +1,10 @@
 import '@/index.css';
 import Layout from '@/components/Layout/Layout.tsx';
-import { AuthProvider } from '@/context/AuthContext';
 
 /**
  * RootLayout - Template Component.
  * @param children - The child components to be rendered within the layout.
  */
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <AuthProvider>
-            <Layout>{children}</Layout>
-        </AuthProvider>
-    );
+    return <Layout>{children}</Layout>;
 }
